e616dfd1331d7647da8e309932423a997f03a5b0
Ahmed-Alfey-Sani <alfey2001sani@gmail.com>
2023-03-24 19:49:35 +0600

    another commit by sani

--- LifeLine/LifeLine/Form1.Designer.cs
+++ LifeLine/LifeLine/Form1.Designer.cs
@@ -88,6 +88,7 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "Click Here to avail our services.\r\n";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
