7698e436a8fcd96abc7e096b46fa226a4ee304ae
Ahmed-Alfey-Sani <alfey2001sani@gmail.com>
2023-04-27 15:35:20 +0600

    bruh

---
+++ LifeLine/LifeLine/Home two.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LifeLine
+{
+    public partial class Home_two : Form
+    {
+        public Home_two()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
